b103bab0202c29f8267ec9808d4beecce7602452
Lens Huygh <lens.huygh@gmail.com>
2019-05-23 10:46:16 +0200

    added second line of text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("main");
+        add2ndLine();
+    }
+
+    private static void add2ndLine() {
+        System.out.println("this is a second line of text");
     }
 }
