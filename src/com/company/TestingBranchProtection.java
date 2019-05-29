c0dc2c0e8ffd58ad5f92de6e04418e791d28644b
lens <lens.huygh@gmail.com>
2019-05-29 20:14:51 +0200

    added TestingBranchProtection (dummy) class

---
+++ src/com/company/TestingBranchProtection.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class TestingBranchProtection {
+    public static void main(String[] args) {
+        System.out.println("testing branch protection dummy");
+    }
+}
