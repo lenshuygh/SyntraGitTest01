6b8f225d27f9aba5d4981e6fc39512b6231f78d6
lens <lens.huygh@gmail.com>
2019-05-29 20:49:40 +0200

    BranchProtectingTestPrToMichiel

---
+++ src/com/company/BranchProtectingTestPrToMichiel.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class BranchProtectingTestPrToMichiel {
+    public static void main(String[] args) {
+        System.out.println("BranchProtectingTestPrToMichiel: see if a new branch can be merged to master on the remote via PR");
+    }
+}
